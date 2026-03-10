7aa8c46dab95b12d624fe1c556f4b4b1041a0dd7
szymon <szymoncyg1@gmail.com>
2026-03-10 18:09:41 +0100

    Dodanie folderu raw, a w nim pliku json. Przygotowanie modelu danych i activity_main.xml

---
+++ app/src/main/java/com/example/mobile_smart_pantry_project_iv/Product.kt
@@ -0,0 +1,9 @@
+package com.example.mobile_smart_pantry_project_iv
+
+class Product (
+    val  id: Int,
+    val nazwa: String,
+    val ilosc: Double,
+    val jednostka: String,
+    val kategoria: String
+)
